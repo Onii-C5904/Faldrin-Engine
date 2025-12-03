a5c97c1086dcc73765ee103f2d83f7898fdb1ea1
Kristijan <kikoismlg123@gmail.com>
2025-12-02 19:07:26 -0500

    Added second map Added animated tiles Updated render code

---
+++ assets/maps/Premium Charakter Spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Premium Charakter Spritesheet" tilewidth="16" tileheight="16" tilecount="1728" columns="24">
+ <image source="../Characters/Premium Charakter Spritesheet.png" width="384" height="1152"/>
+</tileset>
